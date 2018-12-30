f5d586846cdfa5cab2787c6dbdf0054178a35aba
haripriya312 <43338175+haripriya312@users.noreply.github.com>
2018-12-30 11:45:21 +0530

    Update and rename README.md to Biggestnumber.c

---
+++ Biggestnumber.c
@@ -0,0 +1,21 @@
+#include<studio.h>
+#i#include<conio.h>
+int main()
+{
+int a,b,c;
+printf("enter the value of 3 numbers:");
+scanf("%d %d %d",&a,&b,&c);
+if((a>b)&&(a>c))
+{
+printf(" a is bigger",a);
+}
+if else ((b>a)&&(b>c))
+{
+printf(" b is bigger",b);
+}
+else
+{
+printf(" c is bigger",c);
+}
+return 0;
+}
