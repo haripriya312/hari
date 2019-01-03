80b01eabf33f549c23b876ed3604823a7e72d522
haripriya312 <43338175+haripriya312@users.noreply.github.com>
2019-01-03 14:14:32 +0530

    Create power of number.c

---
+++ power of number.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int num,exponent;
+printf("enter the number:");
+scanf("%d",&num);
+printf("enter the exponent:");
+scanf("%d",&exponent);
+for(i=1;i<exponent;i++)
+{
