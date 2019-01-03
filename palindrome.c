0fe3a95b53e9608ef88073218ac82651f6d4a0bd
haripriya312 <43338175+haripriya312@users.noreply.github.com>
2019-01-03 14:36:49 +0530

    Create palindrome.c

---
+++ palindrome.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int num,rem,
+printf("enter a number:");
+scanf("%d",&num);
+temp=num;
+while(temp!=0)
+{
+rem=temp%10;
+rev=rev*10+rem;
+temp=temp/10;
+}
+if(rev==num)
+{
+printf("palindrome");
+}
+else
+{
+printf("not palindrome");
+}
+return 0;
+}
