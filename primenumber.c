9b75f8fd01788fa26b16d382b50bf0bc490cb155
haripriya312 <43338175+haripriya312@users.noreply.github.com>
2019-01-03 14:49:52 +0530

    Create primenumber.c

---
+++ primenumber.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int num,i,count=0;
+printf("enter a number:");
+scanf("%d",&num);
+for(i=1;i<n;i++)
+{
+if(n%i==0)
+{
+count++;
+}
+}
+if(count==2)
+{
+printf("n is a Prime number");
+}
+else
+{
+printf("n is not a Prime number");
+}
+return 0;    
+}
