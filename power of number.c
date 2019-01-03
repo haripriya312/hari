0420d0140bb44fd09fa3d905454fd1483bdbf4bf
haripriya312 <43338175+haripriya312@users.noreply.github.com>
2019-01-03 14:17:35 +0530

    Update power of number.c

--- power of number.c
+++ power of number.c
@@ -2,10 +2,15 @@
 #include<conio.h>
 int main()
 {
-int num,exponent;
+int num,exponent,i;
 printf("enter the number:");
 scanf("%d",&num);
 printf("enter the exponent:");
 scanf("%d",&exponent);
 for(i=1;i<exponent;i++)
 {
+power=power*number;
+}
+printf("%d",power);
+return 0;
+}
