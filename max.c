fe45a7dae3fac92eb2c7e97660968831c9dcaa04
haripriya312 <43338175+haripriya312@users.noreply.github.com>
2019-01-29 12:44:11 +0530

    Create max.c

---
+++ max.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+int n,arr[n];
+printf("enter the number of elements:");
+scanf("%d",&n);
+for(i=0;i<n;i++)
+{
+scanf("%d",&arr[n]);
+}
+max=a[0];
+	for(i=1;i<n;i++)
+	{
+	if(max<a[i])
+	{
+		max=a[i];
+	}
+	}
+	printf("%d",max);	
+}
