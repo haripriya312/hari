362f0e720ea045890a72c69f8912314bd5a0230f
haripriya312 <43338175+haripriya312@users.noreply.github.com>
2019-01-31 13:51:21 +0530

    Create minimum.c

---
+++ minimum.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+int n,arr[n];
+printf("enter the number of elements:");
+scanf("%d",&n);
+for(i=0;i<n;i++)
+{
+scanf("%d",&arr[n]);
+}
+min=a[0];
+	for(i=1;i<n;i++)
+	{
+	if(min>a[i])
+	{
+		min=a[i];
+	}
+	}
+	printf("%d",min);	
+}
+return 0;
+}
