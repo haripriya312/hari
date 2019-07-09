c28ffb87cf9e5f7d993951da50efca1797dda773
haripriya312 <43338175+haripriya312@users.noreply.github.com>
2019-07-09 12:17:35 +0530

    Update String.c

--- String.c
+++ String.c
@@ -4,10 +4,10 @@ int main()
 {
   int n,i;
   char s1[20];
-  printf("\nEnter the value of n:");
-  scanf("%d",&n);
   printf("\nEnter the string:");
-  scanf("%s",s1);
+  scanf("%d",s1);
+  printf("\nEnter the value of n:");
+  scanf("%s",&n);
   for(i=0;i<n;i++)
   {
       printf("\n s1",n);  
