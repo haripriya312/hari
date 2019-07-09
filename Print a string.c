80f336b267b1b724742bc87492e20db867f55be2
haripriya312 <43338175+haripriya312@users.noreply.github.com>
2019-07-09 12:14:29 +0530

    Rename String.c to Print a string.c

---
+++ Print a string.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+int main()
+{
+  int n,i;
+ 
+  char s1[20];
+  printf("\nEnter the value of n:");
+  scanf("%d",&n);
+  printf("\nEnter the string:");
+  scanf("%s",s1);
+  for(i=0;i<n;i++)
+  {
+      printf("\n s1",n);
+      
+  }
+  return 0;
+}
